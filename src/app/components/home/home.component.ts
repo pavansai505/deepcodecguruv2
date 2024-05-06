bbc2565f9c6289c2fb11b7b6db9875b027cc0615
barugala pavan sai <pavansaibarugala@gmail.com>
2024-05-06 20:19:54 +0530

    new nav

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { BenefitsComponent } from './benefits/benefits.component';
 import { NumbersDataComponent } from './numbers-data/numbers-data.component';
 import { CoursesBrochureComponent } from './courses-brochure/courses-brochure.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,BenefitsComponent,NumbersDataComponent,CoursesBrochureComponent],
+  imports: [NavbarComponent,BenefitsComponent,NumbersDataComponent,CoursesBrochureComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
