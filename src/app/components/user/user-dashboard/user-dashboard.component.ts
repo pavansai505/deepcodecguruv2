694b21b23c632de94e6d047932e949f8b1f61447
barugala pavan sai <pavansaibarugala@gmail.com>
2024-05-14 23:12:55 +0530

    a

--- src/app/components/user/user-dashboard/user-dashboard.component.ts
+++ src/app/components/user/user-dashboard/user-dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../utilities/navbar/navbar.component';
 import { FooterComponent } from '../../utilities/footer/footer.component';
-import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-dashboard',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent,RouterOutlet],
+  imports: [NavbarComponent,FooterComponent,RouterOutlet,RouterLink],
   templateUrl: './user-dashboard.component.html',
   styleUrl: './user-dashboard.component.css'
 })
